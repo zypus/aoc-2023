864e77e4d8814981f36dd3f07bd99c2539813d29
Fabian Fränz <7341484+zypus@users.noreply.github.com>
2023-12-03 00:03:07 +0100

    Improving template

--- src/day00/Day00.kt
+++ src/day00/Day00.kt
@@ -14,7 +14,8 @@ fun part2(input: List<String>): Int {
 
 fun main() = AoCTask("day00").run {
     // test if implementation meets criteria from the description, like:
-    check(part1(testInput) == 1)
+    check(part1(testInput), 1)
+    //check(part2(testInput), 1)
 
     println(part1(input))
     println(part2(input))
